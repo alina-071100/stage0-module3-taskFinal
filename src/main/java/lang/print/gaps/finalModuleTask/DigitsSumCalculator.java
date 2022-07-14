f0c1f68655f7dad0cabf1591cd433bdbed6c4b77
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-14 15:48:43 +0400

    FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,6 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int fourth = number/1000;
+        int third = (number - fourth * 1000) / 100;
+        int second = (number - fourth * 1000 - third * 100) / 10;
+        int first =(number - fourth * 1000 - third * 100) - second * 10;
 
+        System.out.println(first + second + third + fourth);
     }
 }
+//4567
+
+// 4 , 5, 6,
