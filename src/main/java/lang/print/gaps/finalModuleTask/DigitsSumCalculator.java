6b07aed3b6136f28bf55c585a678d8138a635f7e
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-14 15:31:04 +0400

    FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int third = number/100; //4
+        int second = (number - third * 100) / 10; //8
+        int first = (number - third * 100) - (second * 10);
+// 489 - 4 *100   - 80 = 9
+
+        System.out.println(first * 100 + second * 10 + third);
     }
 }
